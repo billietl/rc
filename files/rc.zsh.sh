ebe31ea4963e3363151016edd3e79670a10f023c
Louis Billiet <billiet.l@sfeir.com>
2021-07-07 15:28:39 +0200

    up

--- files/rc.zsh.sh
+++ files/rc.zsh.sh
@@ -1,4 +1,7 @@
 #!/bin/zsh
 
+source /usr/share/oh-my-zsh/oh-my-zsh.sh
 ZSH_THEME="powerlevel10k/powerlevel10k"
 source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
+source /usr/share/fzf/key-bindings.zsh
+source /usr/share/fzf/completion.zsh
