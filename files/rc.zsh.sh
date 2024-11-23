28d7b88551064f18dd57b9b38b385a25d3abff25
Louis Billiet <louis@louis.mushland.xyz>
2024-11-23 12:37:58 +0100

    update

--- files/rc.zsh.sh
+++ files/rc.zsh.sh
@@ -2,6 +2,8 @@
 
 source /usr/share/oh-my-zsh/oh-my-zsh.sh
 eval "$(starship init zsh)"
+{% if "zoxide" in modules_to_manage %}
 source /usr/share/fzf/key-bindings.zsh
 source /usr/share/fzf/completion.zsh
+{% endif %}
 unsetopt share_history
